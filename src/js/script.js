aa80df74f4a1267f4ae957a2a6c85db2e23958ac
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-03-06 00:59:01 -0300

    Agregando funcionalidad para cerrar la ventana.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
+var gui = require('nw.gui');
+
 jQuery(window).load(function() {
    // Page Preloader
    jQuery('#status').fadeOut();
    jQuery('#preloader').delay(350).fadeOut(function(){
       jQuery('body').delay(350).css({'overflow':'visible'});
+   });
+
+   jQuery('#imagen-cerrar').click(function(e) {
+     var win = gui.Window.get();
+     win.close();
    });
 });
 
@@ -10,7 +17,6 @@ jQuery(window).load(function() {
 document.onkeydown = function(e) {
   // si pulsa la tecla ESC alterna entre ventana y pantalla completa.
   if (e.keyCode === 27) {
-    var gui = require('nw.gui');
     var win = gui.Window.get();
     win.toggleFullscreen();
   }
@@ -23,33 +29,34 @@ document.onkeydown = function(e) {
 
 
 
-
 //Aquí colocamos el PATH tanto para Huayra como para Windows
 $(document).ready(function() {
 	var OSPath="Unknown OS";
-	
-	if (navigator.appVersion.indexOf("Win") != -1) 
+
+	if (navigator.appVersion.indexOf("Win") != -1)
 		OSPath="file:///F:/recursos/";
 
-	if (navigator.appVersion.indexOf("Linux") != -1) 
-		OSPath="file:///home/alumno/proyectos/recursos/";
+	if (navigator.appVersion.indexOf("Linux") != -1)
+		OSPath="/home/alumno/proyectos/recursos/";
 		//OSPath="file:///media/alumno/DATOS/recursos/";
+
+
+  console.log(OSPath);
 
 	$('.recurso-src').each(function(){
 		$(this).attr('src',OSPath+$(this).attr('src'))
 	});
-	
+
 	$('.recurso-poster').each(function(){
 		$(this).attr('poster',OSPath+$(this).attr('poster'))
 	});
-	
+
 	$('.recurso-href').each(function(){
 		$(this).attr('href',OSPath+$(this).attr('href'))
 	});
-	
+
 	$('.playlist').each(function(){
 		$(this).attr('data-video',OSPath+$(this).attr('data-video'));
 		$(this).attr('data-track',OSPath+$(this).attr('data-track'));
 	});
 });
-
