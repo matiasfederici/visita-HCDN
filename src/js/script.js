d2edaa6f6bcefdfe4680c932556fbb1167aec937
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-03-05 23:09:24 -0300

    arreglos menores.

--- src/js/script.js
+++ src/js/script.js
@@ -6,16 +6,34 @@ jQuery(window).load(function() {
    });
 });
 
+
+document.onkeydown = function(e) {
+  // si pulsa la tecla ESC alterna entre ventana y pantalla completa.
+  if (e.keyCode === 27) {
+    var gui = require('nw.gui');
+    var win = gui.Window.get();
+    win.toggleFullscreen();
+  }
+
+
+
+  // Si pulsa la tecla "i" abre las herramientas de desarrollo.
+
+};
+
+
+
+
 //Aquí colocamos el PATH tanto para Huayra como para Windows
 $(document).ready(function() {
 	var OSPath="Unknown OS";
 	
-	if (navigator.appVersion.indexOf("Win")!=-1) 
+	if (navigator.appVersion.indexOf("Win") != -1) 
 		OSPath="file:///F:/recursos/";
-	if (navigator.appVersion.indexOf("Linux")!=-1) 
-		OSPath="file:///media/alumno/DATOS/recursos/";
-	if (navigator.appVersion.indexOf("X11")!=-1) 
-		OSPath="file:///media/alumno/DATOS/recursos/";
+
+	if (navigator.appVersion.indexOf("Linux") != -1) 
+		OSPath="file:///home/alumno/proyectos/recursos/";
+		//OSPath="file:///media/alumno/DATOS/recursos/";
 
 	$('.recurso-src').each(function(){
 		$(this).attr('src',OSPath+$(this).attr('src'))
